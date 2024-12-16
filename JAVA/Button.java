4b19ca2252185278a2187fece80231606325d3e8
MrAdi149 <singhadityakumar1911@gmail.com>
2024-12-16 18:19:36 +0530

    Java disscussed topic: Reference Variable, create button class and print the refernce variable

---
+++ JAVA/Button.java
@@ -0,0 +1,13 @@
+//define a class Button, define main method and create 2 objects of button class print the reference-variables.
+class Button {
+
+	public static void main(String[] args){
+
+		Button b1 = new Button();
+		System.out.println(b1);
+		Button b2 = new Button();
+		System.out.print(b2);
+
+	}
+
+}
